3ef6d605e8d8d7ec21d95ca1fa05183940e788e8
Guest User <Guest@Epicodus-6E.local>
2018-03-30 09:56:04 -0700

    create models and class ToDo and added class to app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToDo } from './models/to-do.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Weather Or Not';
+  masterToDo: ToDo[] = [
+    new ToDo("Sky Diving", "Go to the drop-zone and jump out", "raining", 38, false),
+    new ToDo("Study", "Go to the library and read and write Code", "raining", 38, false)
+  ];
 }
---
+++ src/app/models/to-do.model.ts
@@ -0,0 +1,3 @@
+export class ToDo {
+  constructor(public task: string, public description: string, public conditions: string, public temperature: number, public suggest: boolean){}
+}
