59ef967489d5749d5adacbe06fb8825b46729a22
Guest User <Guest@Epicodus-6E.local>
2018-03-30 16:54:34 -0700

    add edit html

--- src/app/models/to-do.model.ts
+++ src/app/models/to-do.model.ts
@@ -1,3 +1,7 @@
 export class ToDo {
   constructor(public task: string, public description: string, public conditions: string, public temperature: number, public suggest: boolean){}
 }
+
+//
+// let request = new XMLHttpRequest();
+// let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f41bec18f9c4fb89d5f3987aac74166d`
